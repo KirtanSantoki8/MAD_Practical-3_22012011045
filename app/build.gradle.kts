5f859e03e0847bab3d5b3f34e7743e8d44de1d53
KirtanPatel8 <kirtansantoki12@gmail.com>
2024-08-16 11:02:21 +0530

    Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.devkt.mad_practical_3_22012011045"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
